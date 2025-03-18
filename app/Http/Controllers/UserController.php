965b51998eaf2b05d37c10f12feaf07f9e7cb975
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2025-03-18 20:37:24 +0700

    JS6_Praktikum 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -257,6 +257,26 @@ class UserController extends Controller
         $user = UserModel::find($id);
         return view('user.confirm_ajax', ['user' => $user]);
     }
+
+    public function delete_ajax(Request $request, $id)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
     // public function tambah(){
     //     return view('user_tambah');
     // }
