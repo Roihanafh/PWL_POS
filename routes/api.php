04ca058e3a6d59e40219c251b7a2d796ca4855dc
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2025-05-05 14:26:25 +0700

    JS10_Praktikum 1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/register', RegisterController::class)->name('register');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
