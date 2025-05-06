956bc026fe0616f284e5617914dd3ab034efc7dd
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2025-05-06 07:01:20 +0700

    JS10_Praktikum 4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;//implementasi class authenticatable
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
@@ -45,5 +46,11 @@ class UserModel extends Authenticatable implements JWTSubject
     public function getRole()
     {
         return $this->level->level_kode;
+    }
+    protected function image():Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('storage/posts/'.$image),
+        );
     }
 }
